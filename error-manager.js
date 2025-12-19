ba205c535e906cb25831e245a8ce20e1dbdea117
Farzadseb <farzad.seb@gmail.com>
2025-12-19 12:10:26 +0330

    Create error-manager.js

---
+++ error-manager.js
@@ -0,0 +1,14 @@
+// error-manager.js
+export class ErrorManager {
+    static log(error, context = {}) {
+        // لاگ کردن خطا
+    }
+    
+    static notify(error, level = 'error') {
+        // نمایش به کاربر
+    }
+    
+    static recover(error) {
+        // تلاش برای ریکاوری
+    }
+}
